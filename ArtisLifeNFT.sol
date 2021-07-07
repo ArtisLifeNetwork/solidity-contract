af04a50abdd481a48057e62e5962210af9384712
Adrian <adrianv425@gmail.com>
2021-07-07 12:10:48 -0600

    added comments and fixed formating

--- ArtisLifeNFT.sol
+++ ArtisLifeNFT.sol
@@ -22,95 +22,6 @@ contract ArtisLifeNFT is ERC1155PresetMinterPauser {
         uint256 edition,
         uint256 value
     );
-
-    constructor()
-        ERC1155PresetMinterPauser("https://game.example/api/item/{id}.json")
-    {
-        _setupRole(MINTER_ROLE, msg.sender);
-        _setupRole(OWNER_ROLE, msg.sender);
-        name = "ArtisLife Network Minter";
-        symbol = "ARTISNFT";
-    }
-
-    modifier onlyOwner() {
-        require(hasRole(OWNER_ROLE, msg.sender), "Sender not Govern");
-        _;
-    }
-
-    modifier onlyMinter() {
-        require(hasRole(MINTER_ROLE, msg.sender), "Sender not minter");
-        _;
-    }
-
-    function pause() public override {
-        require(false);
-    }
-
-    function unpause() public override {
-        require(false);
-    }
-
-    function mint(
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) public override {
-        require(false, "Use 5 arg mintFor");
-    }
-
-    function mintBatch(
-        address to,
-        uint256[] calldata ids,
-        uint256[] calldata amounts,
-        bytes memory data
-    ) public override {
-        require(false);
-    }
-
-    function mintEditionBatch(
-        address to,
-        uint256[] calldata ids,
-        uint256[] calldata amounts,
-        uint256[] calldata editions,
-        bytes memory data
-    ) public onlyMinter() {
-        for (uint256 i = 0; i < ids.length; i++) {
-            mintFor(to, ids[i], amounts[i], editions[i], abi.encode(i));
-        }
-    }
-
-    function createNewNFT(
-        string calldata name,
-        string calldata symbol,
-        uint256 totalSupply,
-        address creator
-    ) public virtual onlyOwner() {
-        nftIDs[nftCount] = true;
-        emit NFTCreated(_tokenIds, creator);
-        nftCount = nftCount.add(1);
-        _tokenIds = _tokenIds.add(1);
-    }
-
-    function giveRole(address account) public onlyOwner() {
-        grantRole(MINTER_ROLE, account);
-    }
-
-    function mintFor(
-        address to,
-        uint256 id,
-        uint256 amount,
-        uint256 edition,
-        bytes memory data
-    ) public virtual onlyMinter() {
-        require(nftIDs[id] != false, "NFT doesn't exist");
-        emit TransferSingleEdition(msg.sender, address(0), to, id, edition, 1);
-        _mint(to, id, amount, abi.encode(edition));
-    }
-}
-
-contract ArtisLifeNFTEditions is ArtisLifeNFT {
-    using SafeMath for uint256;
     mapping(uint256 => nft_data) nftData;
     uint256 public totalSupply;
     struct nft_data {
@@ -122,39 +33,66 @@ contract ArtisLifeNFTEditions is ArtisLifeNFT {
         mapping(uint256 => address) editionOwnerships;
     }
 
+    constructor()
+        ERC1155PresetMinterPauser("https://game.example/api/item/{id}.json")
+    {
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(OWNER_ROLE, msg.sender);
+        name = "ArtisLife Network Minter";
+        symbol = "ARTISNFT";
+    }
+
+    /*
+        onlyOwner
+
+        Function to check if the sender of the transaction is
+        the owner of this contract.
+    */
+    modifier onlyOwner() {
+        require(hasRole(OWNER_ROLE, msg.sender), "Sender not Govern");
+        _;
+    }
+
+    /*
+        onlyMinter
+
+        Function to check if the sender of the transaction has
+        minting priviledges.
+    */
+    modifier onlyMinter() {
+        require(hasRole(MINTER_ROLE, msg.sender), "Sender not minter");
+        _;
+    }
+
+    /*
+        giveRole
+
+        Admin function to give the farming contract minting
+        rights to all NFTs. If contract is updated, admin will
+        use this function to update accordingly.
+    */
+    function giveRole(address account) public onlyOwner() {
+        grantRole(MINTER_ROLE, account);
+    }
+
+    /*
+        hasBalance
+
+        Function to check if the requested account address has an
+        NFT balance of the provided NFT id.
+    */
     modifier hasBalance(uint256 id, address sender) {
         require(nftData[id].balanceOf[sender] > 0, "No NFTs");
         _;
     }
 
-    function createNewNFT(
-        string calldata name,
-        string calldata symbol,
-        uint256 totalNFTSupply,
-        address creator
-    ) public override {
-        totalSupply = totalSupply.add(1);
-        nftData[_tokenIds].name = name;
-        nftData[_tokenIds].symbol = symbol;
-        nftData[_tokenIds].TOTAL_SUPPLY = totalNFTSupply;
-        super.createNewNFT(name, symbol, totalNFTSupply, creator);
-    }
+    /*
+        balanceOfEdition
 
-    function mintFor(
-        address to,
-        uint256 id,
-        uint256 amount,
-        uint256 edition,
-        bytes memory data
-    ) public override {
-        require(nftData[id].TOTAL_SUPPLY > edition, "Edition not availble");
-        require(nftData[id].editionsMinted[edition] == false, "Edition minted");
-        nftData[id].editionsMinted[edition] = true;
-        nftData[id].editionOwnerships[edition] = to;
-        nftData[id].balanceOf[to] = nftData[id].balanceOf[to].add(1);
-        super.mintFor(to, id, amount, edition, data);
-    }
-
+        Preferred balanceOf function. Returns the balance of total NFTs 
+        owned by the provided account address. 
+        Edition numbers are preserved.
+    */
     function balanceOfEdition(
         address account,
         uint256 id,
@@ -163,6 +101,39 @@ contract ArtisLifeNFTEditions is ArtisLifeNFT {
         return nftData[id].editionOwnerships[edition] == account;
     }
 
+    /*
+        balanceOfEditionBatch
+
+        Preferred balanceOfBatch function. Returns a batch of the balances
+        of total NFTs owned by the provide account addresses. 
+        Edition numbers are preserved.
+    */
+    function balanceOfEditionBatch(
+        address[] calldata accounts,
+        uint256[] calldata ids,
+        uint256[] calldata editions
+    ) public view returns (bool[] memory) {
+        require(
+            ids.length == accounts.length,
+            "ERC1155: ids and amounts length mismatch"
+        );
+        bool[] memory batchBalances = new bool[](accounts.length);
+        for (uint256 i = 0; i < ids.length; i++) {
+            batchBalances[i] = this.balanceOfEdition(
+                accounts[i],
+                ids[i],
+                editions[i]
+            );
+        }
+        return batchBalances;
+    }
+
+    /*
+        balanceOf
+
+        Returns the balance of total NFTs owned by the provided
+        account address. Edition numbers are not returned.
+    */
     function balanceOf(address account, uint256 id)
         public
         view
@@ -172,6 +143,12 @@ contract ArtisLifeNFTEditions is ArtisLifeNFT {
         return nftData[id].balanceOf[account];
     }
 
+    /*
+        balanceOfBatch
+
+        Returns a batch of the balances of total NFTs owned by the provided
+        account addresses. Edition numbers are not returned.
+    */
     function balanceOfBatch(address[] calldata accounts, uint256[] calldata ids)
         public
         view
@@ -189,6 +166,12 @@ contract ArtisLifeNFTEditions is ArtisLifeNFT {
         return batchBalances;
     }
 
+    /*
+        highestEdition
+
+        Looks for the highest edition number of NFT id owned
+        by the sender's address.
+    */
     function highestEdition(address sender, uint256 id)
         private
         view
@@ -200,22 +183,173 @@ contract ArtisLifeNFTEditions is ArtisLifeNFT {
         revert("No editions");
     }
 
-    function safeBatchTransferFrom(
-        address sender,
-        address receiver,
+    /*
+        createNewNFT
+
+        Creates an NFT entry without minting any NFTs.
+        Establishes name, symbol, totalSupply and creator.
+    */
+    function createNewNFT(
+        string calldata name,
+        string calldata symbol,
+        uint256 totalNFTSupply,
+        address creator
+    ) public virtual onlyOwner() {
+        totalSupply = totalSupply.add(1);
+        nftData[_tokenIds].name = name;
+        nftData[_tokenIds].symbol = symbol;
+        nftData[_tokenIds].TOTAL_SUPPLY = totalNFTSupply;
+        nftIDs[nftCount] = true;
+        emit NFTCreated(_tokenIds, creator);
+        nftCount = nftCount.add(1);
+        _tokenIds = _tokenIds.add(1);
+    }
+
+    /*
+        mintFor
+        
+        Default mint function that requires the edition as a
+        parameter. Only one address is allowed to call
+        this function. Checks to see if the edition can or has
+        been minted.
+    */
+    function mintFor(
+        address to,
+        uint256 id,
+        uint256 amount,
+        uint256 edition,
+        bytes memory data
+    ) public virtual onlyMinter() {
+        require(nftIDs[id] != false, "NFT doesn't exist");
+        require(nftData[id].TOTAL_SUPPLY > edition, "Edition not availble");
+        require(nftData[id].editionsMinted[edition] == false, "Edition minted");
+        nftData[id].editionsMinted[edition] = true;
+        nftData[id].editionOwnerships[edition] = to;
+        nftData[id].balanceOf[to] = nftData[id].balanceOf[to].add(1);
+        emit TransferSingleEdition(msg.sender, address(0), to, id, edition, 1);
+        _mint(to, id, amount, abi.encode(edition));
+    }
+
+    /*
+        mintEditionBatch
+
+        Default mintBatch function that requires the edition as
+        a parameter. Only one address is allowed to call this
+        function. Calls mintFor function for the entire batch.
+    */
+    function mintEditionBatch(
+        address to,
         uint256[] calldata ids,
         uint256[] calldata amounts,
-        bytes calldata data
-    ) public override {
-        require(
-            ids.length == amounts.length,
-            "ERC1155: ids and amounts length mismatch"
-        );
+        uint256[] calldata editions,
+        bytes memory data
+    ) public onlyMinter() {
         for (uint256 i = 0; i < ids.length; i++) {
-            this.safeTransferFrom(sender, receiver, ids[i], amounts[i], data);
+            mintFor(to, ids[i], amounts[i], editions[i], abi.encode(i));
         }
     }
 
+    /*
+        mint
+
+        DISABLED. We only allowing minting through mintFor and
+        mintEditionBatch.
+    */
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public override {
+        require(false, "Use 5 arg mintFor");
+    }
+
+    /*
+        mintBatch
+
+        DISABLED. We only allowing minting through mintFor and
+        mintEditionBatch.
+    */
+    function mintBatch(
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes memory data
+    ) public override {
+        require(false, "Use 5 arg mintEditionBatch");
+    }
+
+    /*
+        safeEditionTransferFrom
+
+        The prefered transfer method. If a user owns
+        more than one edition, they can specify which
+        edition they would like to send. Even if the user
+        owns one edition, this transfer method will use
+        less gas than safeTransferFrom()
+    */
+    function safeEditionTransferFrom(
+        address sender,
+        address receiver,
+        uint256 id,
+        uint256 amount,
+        uint256 edition,
+        bytes calldata data
+    ) public hasBalance(id, sender) {
+        require(nftData[id].editionOwnerships[edition] == sender, "Not Owner");
+        emit TransferSingleEdition(
+            msg.sender,
+            sender,
+            receiver,
+            id,
+            edition,
+            1
+        );
+        nftData[id].editionOwnerships[edition] = receiver;
+        nftData[id].balanceOf[sender] -= 1;
+        nftData[id].balanceOf[receiver] += 1;
+        super.safeTransferFrom(sender, receiver, id, amount, data);
+    }
+
+    /*
+        safeTransferFrom
+
+        Override existing transfer function to add functionality of
+        preserving edition numbers. If multiple editions are owned, the
+        highest edition will be sent
+    */
+    function safeTransferFrom(
+        address sender,
+        address receiver,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) public override hasBalance(id, sender) {
+        uint256 edition = highestEdition(sender, id);
+        require(nftData[id].editionOwnerships[edition] == sender, "Not Owner");
+        emit TransferSingleEdition(
+            msg.sender,
+            sender,
+            receiver,
+            id,
+            edition,
+            1
+        );
+        nftData[id].editionOwnerships[edition] = receiver;
+        nftData[id].balanceOf[sender] -= 1;
+        nftData[id].balanceOf[receiver] += 1;
+        super.safeTransferFrom(sender, receiver, id, amount, data);
+    }
+
+    /*
+        safeBatchEditionTransferFrom
+
+        The prefered batchTransfer method. If a user owns
+        more than one edition, they can specify which
+        edition they would like to send. Even if the user
+        owns one edition, this transfer method will use
+        less gas than safeBatchTransferFrom()
+    */
     function safeBatchEditionTransferFrom(
         address sender,
         address receiver,
@@ -240,49 +374,44 @@ contract ArtisLifeNFTEditions is ArtisLifeNFT {
         }
     }
 
-    function safeTransferFrom(
+    /*
+        safeBatchTransferFrom
+
+        Override existing batchTransfer function to add functionality of
+        preserving edition numbers. If multiple editions are owned, the
+        highest edition will be sent
+    */
+    function safeBatchTransferFrom(
         address sender,
         address receiver,
-        uint256 id,
-        uint256 amount,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
         bytes calldata data
-    ) public override hasBalance(id, sender) {
-        uint256 edition = highestEdition(sender, id);
-        require(nftData[id].editionOwnerships[edition] == sender, "Not Owner");
-        emit TransferSingleEdition(
-            msg.sender,
-            sender,
-            receiver,
-            id,
-            edition,
-            1
+    ) public override {
+        require(
+            ids.length == amounts.length,
+            "ERC1155: ids and amounts length mismatch"
         );
-        nftData[id].editionOwnerships[edition] = receiver;
-        nftData[id].balanceOf[sender] -= 1;
-        nftData[id].balanceOf[receiver] += 1;
-        super.safeTransferFrom(sender, receiver, id, amount, data);
+        for (uint256 i = 0; i < ids.length; i++) {
+            this.safeTransferFrom(sender, receiver, ids[i], amounts[i], data);
+        }
     }
 
-    function safeEditionTransferFrom(
-        address sender,
-        address receiver,
-        uint256 id,
-        uint256 amount,
-        uint256 edition,
-        bytes calldata data
-    ) public hasBalance(id, sender) {
-        require(nftData[id].editionOwnerships[edition] == sender, "Not Owner");
-        emit TransferSingleEdition(
-            msg.sender,
-            sender,
-            receiver,
-            id,
-            edition,
-            1
-        );
-        nftData[id].editionOwnerships[edition] = receiver;
-        nftData[id].balanceOf[sender] -= 1;
-        nftData[id].balanceOf[receiver] += 1;
-        super.safeTransferFrom(sender, receiver, id, amount, data);
+    /*
+        pause
+
+        DISABLED. Reason: reduce contract size
+    */
+    function pause() public override {
+        require(false);
+    }
+
+    /*
+        unpause
+
+        DISABLED. Reason: reduce contract size
+    */
+    function unpause() public override {
+        require(false);
     }
 }
